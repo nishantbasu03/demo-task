e1a9820a2196cec18582d20ecd24d1622df59521
nishantbasu03 <nishantbasu3@gmail.com>
2022-08-06 09:26:14 +0530

    create task

--- src/app/task-deshboard/task-deshboard.component.ts
+++ src/app/task-deshboard/task-deshboard.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CommonService } from './common.service';
 
 @Component({
   selector: 'app-task-deshboard',
   templateUrl: './task-deshboard.component.html',
   styleUrls: ['./task-deshboard.component.scss']
 })
-export class TaskDeshboardComponent {
+export class TaskDeshboardComponent implements OnInit{
   public taskDeshoardList:any = {
     data : [
       {
@@ -14,10 +16,11 @@ export class TaskDeshboardComponent {
         noOfTask : 1,
         taskList:[
           {
+            id: 1,
             title : 'Design',
             description : "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
             priority: 1,
-            image: ''
+            fileSource: ''
           }
         ]
       },
@@ -26,10 +29,11 @@ export class TaskDeshboardComponent {
         noOfTask : 1,
         taskList:[
           {
+            id: 2,
             title : 'Development',
             description : "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
             priority: 2,
-            image: ''
+            fileSource: ''
           }
         ]
       },
@@ -40,14 +44,43 @@ export class TaskDeshboardComponent {
       }
     ]
   }
+  private isAddItemSubscription: Subscription | undefined;
+  public taskCount:any = 3;
 
-  constructor(private router: Router){}
+  constructor(private router: Router,
+    private commonService: CommonService){}
+
+  ngOnInit(){
+    this.getCreatedTask();
+  }
+
+
+  getCreatedTask(){
+    this.isAddItemSubscription = this.commonService.addItemObservable.subscribe((item)=>{
+      if(item && Object.keys(item).length > 0){
+        if(item.title){
+          this.taskCount += 1;
+          this.taskDeshoardList.data.map((tasklist:any) => {
+            if(tasklist.status == 1){
+              tasklist.noOfTask += 1;
+              tasklist.taskList.push(item);
+            }
+          })
+        }
+      }
+    });
+  }
 
   createTask(data:any){
     if(data == 1){
-      this.router.navigate(['/task-deshboard/create-task']);
+      this.router.navigate(['/task-deshboard/create-task'], { queryParams: { id: this.taskCount}});
     }
   }
 
+  ngOnDestroy(){
+    if(this.isAddItemSubscription){
+        this.isAddItemSubscription.unsubscribe();
+    }
+  }
 
 }
